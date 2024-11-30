a8d5f52796958ee723f97958d5a313ad601c1087
kapilansh-10 <129315403+kapilansh-10@users.noreply.github.com>
2024-11-30 13:06:06 +0000

    DataTypes

---
+++ 01basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs and not browser
+
+console.log(3+3)
+
+console.log("Kapilnash")
+
+// Code Readability should be High
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof undefined) // o/p undefined
+console.log(typeof null) // o/p object
