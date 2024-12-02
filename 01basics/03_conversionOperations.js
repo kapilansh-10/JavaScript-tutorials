c91320346d9486e34afa337070991c754cf1927e
kapilansh-10 <129315403+kapilansh-10@users.noreply.github.com>
2024-12-02 14:04:58 +0000

    Operations

--- 01basics/03_conversionOperations.js
+++ 01basics/03_conversionOperations.js
@@ -24,5 +24,39 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************** Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+str1 = "hello"
+str2 = " kapilansh"
+str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num1,num2,num3);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
