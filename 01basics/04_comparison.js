a474a044cc22e1ea831ce5d0c8321805cea2de2c
kapilansh-10 <129315403+kapilansh-10@users.noreply.github.com>
2024-12-03 15:23:03 +0000

    Comparison

---
+++ 01basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// Javascript converts the string in int datatype, but make sure to use same data types only
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === Used for strict checking , as it checks number as well as data type also
+
+console.log("2" === 2);
