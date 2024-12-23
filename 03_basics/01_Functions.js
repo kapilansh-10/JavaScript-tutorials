d5fd6088df35ac3ebfc3cb536246076b5d3ecbb7
kapilansh-10 <129315403+kapilansh-10@users.noreply.github.com>
2024-12-23 10:13:05 +0000

    Functions with objects and array in javascript

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -37,4 +37,50 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Kapilansh"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+//! Functions with Objects and Array
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(3))
+// console.log(calculateCartPrice(200,400,600)) // for this (...num1) is used which is called as rest //
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,600,800,1000)) // Here only 800 and 1000 will get print as these values only get stored in num1,because 200 and 400 are stored in val1 and val2 //
+
+// Now object part
+
+const user = {
+    username: "Kapilansh",
+    price: 399
+}
+
+function handleObject(anyObject){
+    console.log(`The user is ${anyObject.username}, and price is ${anyObject.price}`)
+    return anyObject
+}
+
+// handleObject(user)
+
+// Now passing the object directly
+handleObject({
+    username: "Tanky",
+    price: 100
+})
+
+// Similarly, now passing arrays
+
+const myNewArray = [1000, 2000, 3000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+
+console.log(returnSecondValue([500,1500,4500]))
