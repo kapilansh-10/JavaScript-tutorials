05b65235dcdd7d2904b40b22b823bd74b7e2717e
kapilansh-10 <129315403+kapilansh-10@users.noreply.github.com>
2024-12-24 15:37:58 +0000

    switch statement

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,51 @@
+// Switch Syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("Feb")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "january":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("Feb")
+        break;
+    case "march":
+        console.log("March")
+        break;
+    case "april":
+        console.log("April")
+        break;
+
+    default:
+        break;
+}
