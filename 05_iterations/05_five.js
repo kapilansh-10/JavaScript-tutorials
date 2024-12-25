34b3180324c19c25a403db286d4ccf93ee05b200
kapilansh-10 <129315403+kapilansh-10@users.noreply.github.com>
2024-12-25 15:19:12 +0000

    for each loop

---
+++ 05_iterations/05_five.js
@@ -0,0 +1,58 @@
+// for each loop
+/*
+
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach( function (val) {
+    console.log(val);    
+} )
+
+ */
+
+// Arrow Function
+
+/*
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach( (item) => {
+    console.log(item);    
+} )
+*/
+
+/*
+const coding = ["js","ruby","java","python","cpp"]
+function printme(item){
+    console.log(item);    
+}
+
+coding.forEach(printme)
+*/
+
+/*
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach((item,index,arr) => {
+    console.log(item,index,arr);
+    
+})
+*/
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+})
